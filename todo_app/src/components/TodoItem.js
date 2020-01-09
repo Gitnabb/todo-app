f2312ecf70f25659e2a84f75499edea2f96147bb
Kjetil Hammerseth <kjetilhammerseth@gmail.com>
2020-01-09 09:07:53 +0100

    added components, cleaned a little

---
+++ todo_app/src/components/TodoItem.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export class TodoItem extends Component {
+    render() {
+        return (
+            <div style={itemStyle}>
+                <p>{this.props.todo.title}</p>
+            </div>
+        )
+    }
+}
+
+// PropTypes
+TodoItem.propTypes = {
+    todo: PropTypes.object.isRequired
+}
+
+//
+const itemStyle = {
+    backgroundColor: '#f3f3f3'
+}
+
+export default TodoItem
